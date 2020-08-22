0dc82834c18df769fd6ff7254ceee1ad9a99f822
Karthik <karthik@collect.chat>
2020-08-22 14:09:30 +0530

    Increase gnews list limit to 30

--- src/config/apis.js
+++ src/config/apis.js
@@ -4,4 +4,4 @@ export const WeatherApi = (lat, lon) =>
   `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPEN_WEATHER_KEY}`;
 export const IpApi = 'https://wtfismyip.com/json';
 export const NewsApi = (query, lang, cc = 'us') =>
-  `https://gnews.io/api/v3/search?q=${query}&lang=${lang}&image=true&country=${cc}&token=${GNEWS_KEY}`;
+  `https://gnews.io/api/v3/search?q=${query}&lang=${lang}&image=true&country=${cc}&token=${GNEWS_KEY}&max=30`;
